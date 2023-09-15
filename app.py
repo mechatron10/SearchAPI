3528b5f46092f87684d2a0aa060cbdb7e8e612f8
Mechatron <soni.anurag0761@gmail.com>
2023-09-15 12:55:36 +0530

    app.py updated to avoid the list out of bound error

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 # Configure CORS to allow requests from your local frontend during development
 CORS(app, resources={r"/search": {"origins": "https://frontend-search-api.vercel.app"}})
-cache = Cache(app,config={'CACHE_TYPE': 'simple'})
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 base_url = "https://app.ylytic.com/ylytic/test"
 
 # Fetch and cache comments_data only once when the application starts
@@ -53,15 +53,15 @@ def get_comments():
     for comment in comments_data:
         if isinstance(comment, dict):
             author = comment.get("author", "").lower()
-            at = str(comment.get("at", ""))  
-            like = comment.get("like", 0) 
-            reply = comment.get("reply", 0) 
+            at = str(comment.get("at", ""))
+            like = comment.get("like", 0)
+            reply = comment.get("reply", 0)
             text = comment.get("text", "").lower()
             try:
                 at_datetime = datetime.strptime(at, "%a, %d %b %Y %H:%M:%S %Z")
             except ValueError:
                 at_datetime = None  # Handle invalid 'at' values as None
-             
+
             # Perform comparisons using strings
             # Split both search_text and text into words
             search_words = search_text.lower().split() if search_text else []
@@ -69,7 +69,7 @@ def get_comments():
 
             # Check if search_words appear in the same sequence in comment_words
             if (
-                (search_author is None or search_author.lower() in author) and
+                (search_author is None or (search_author.strip() and search_author.lower() in author)) and
                 (at_from is None or datetime.strptime(at, "%a, %d %b %Y %H:%M:%S %Z") >= datetime.strptime(at_from, "%a, %d %b %Y %H:%M:%S %Z")) and
                 (at_to is None or datetime.strptime(at, "%a, %d %b %Y %H:%M:%S %Z") <= datetime.strptime(at_to, "%a, %d %b %Y %H:%M:%S %Z")) and
                 (like_from is None or like >= int(like_from)) and
@@ -77,7 +77,7 @@ def get_comments():
                 (reply_from is None or reply >= int(reply_from)) and
                 (reply_to is None or reply <= int(reply_to)) and
                 all(word in comment_words for word in search_words) and  # Check if all words in search_text are present in text
-                search_words == [word for word in comment_words[comment_words.index(search_words[0]):comment_words.index(search_words[-1])+1]]
+                ' '.join(search_words) in ' '.join(comment_words)
             ):
                 filtered_comments.append(comment)
 
